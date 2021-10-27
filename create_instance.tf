7efdffed829b1fb79c8adb939caac41239e6d049
Sergey <you@example.com>
2021-10-27 10:36:19 +0300

    add private_key

--- create_instance.tf
+++ create_instance.tf
@@ -62,7 +62,7 @@ resource "yandex_compute_instance" "build" {
       host = self.network_interface[0].nat_ip_address
       type = "ssh"
       user = "ubuntu"
-      key = "extor:${file("~/.ssh/id_rsa.pub")}"
+      private_key = "extor:${file("~/.ssh/id_rsa.pub")}"
     }
   }
 
