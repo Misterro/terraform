df98a8855451a3f3ca68aec7cd5ac62ef102b3a2
samalyshev <samalyshev@sberbank.ru>
2021-10-28 08:06:45 +0300

    new branch

--- create_instance.tf
+++ create_instance.tf
@@ -65,48 +65,6 @@ resource "yandex_compute_instance" "build" {
       private_key = file("~/.ssh/id_rsa")
     }
   }
-
-  provisioner "local-exec" {
-      command = "ssh-keyscan ${self.network_interface[0].nat_ip_address} >> ~/.ssh/known_hosts && apt update && apt install rsync -y && rsync -avzRy Dockerfile ubuntu@${self.network_interface[0].nat_ip_address}:/tmp/"
-    }
-
-  provisioner "remote-exec" {
-    inline = ["apt update",
-      "sudo apt install docker.io -y",
-     "sudo docker build -t box /tmp",
-     "sudo docker login --username oauth --password ${var.yandex-token} cr.yandex",
-     "sudo docker tag box cr.yandex/${yandex_container_registry.registry.id}/box:latest",
-     "sudo docker push cr.yandex/${yandex_container_registry.registry.id}/box:latest"]
-
-    connection {
-      host = self.network_interface[0].nat_ip_address
-      type = "ssh"
-      user = "ubuntu"
-      private_key = file("~/.ssh/id_rsa")
-    }
-  }
-}
-
-resource "null_resource" "previous" {}
-
-resource "time_sleep" "wait_30_seconds" {
-    depends_on = [null_resource.previous]
-
-    create_duration = "30s"
-}
-
-resource "yandex_container_registry" "registry" {
-  name = "registry"
-  folder_id = var.yandex-folder-id
-}
-
-resource "yandex_container_registry_iam_binding" "user" {
-  registry_id = yandex_container_registry.registry.id
-  role = "container-registry.images.pusher"
-
-  members = [
-    "userAccount:ajecrgtho5m706hs6ej0"
-  ]
 }
 
 resource "yandex_compute_instance" "prod" {
@@ -130,22 +88,4 @@ resource "yandex_compute_instance" "prod" {
   metadata = {
     ssh-keys = "extor:${file("~/.ssh/id_rsa.pub")}"
   }
-
-  provisioner "remote-exec" {
-    inline = ["apt update",
-      "sudo apt install docker.io -y",
-     "sudo docker volume create --name volume", 
-     "sudo docker login --username oauth --password ${var.yandex-token} cr.yandex", 
-     "sudo docker run -d -v volume:/war cr.yandex/${yandex_container_registry.registry.id}/box:latest",
-     "sudo docker run -d -v volume:/usr/local/tomcat/webapps -p 8085:8080 tomcat:9.0.20-jre8-alpine",
-     "sudo docker ps -a"]
-
-    connection {
-      host = self.network_interface[0].nat_ip_address
-      type = "ssh"
-      user = "ubuntu"
-      private_key = file("~/.ssh/id_rsa")
-    }
-  }
-  depends_on = [yandex_compute_instance.build]
 }
